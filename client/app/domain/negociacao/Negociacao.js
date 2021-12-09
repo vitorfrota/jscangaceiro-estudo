266ae6e1dc7b406ba6859817b3164407a5449ffe
Vitor <vitorfrota@live.com>
2021-12-09 15:11:38 -0400

    criar domínio(modelo) de negociação

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,23 @@
+class Negociacao {
+    constructor(data, quantidade, valor){
+        this._data = data; // data atual
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    getVolume(){
+        return this._quantidade * this._valor;
+    }
+
+    getData(){
+        return this._data;
+    }
+
+    getQuantidade(){
+        return this._quantidade;
+    }
+
+    getValor(){
+        return this._valor;
+    }
+}
