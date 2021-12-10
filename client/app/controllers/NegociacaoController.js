27c95a49805156c9d34908f2129e6f07e9ff1c88
Vitor Frota <vitorfrota@live.com>
2021-12-10 15:09:20 -0400

    Atribuindo as responsabilidades no controller e criando um modelo para as negociacoes

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -4,20 +4,28 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
     adiciona(event){
         event.preventDefault();
 
-        let converter = new DateConverter();
+        this._negociacoes.adiciona(this._criaNegociacao());
 
-        let data = new Date(converter.paraData(this._inputData.value));
+        this._limpaFormulario();
+    }
 
-        let negociacao = new Negociacao(
-            data,
+    _criaNegociacao(){
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         )
-        
-        console.log(negociacao);
+    }
+
+    _limpaFormulario(){
+        this._inputData.value = "";
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
