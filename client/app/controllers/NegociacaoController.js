9cbb709345b39a99d1c15524816584f56af324aa
Vitor Frota <vitorfrota@live.com>
2021-12-15 15:38:02 -0400

    correção input date para outros navegadores e criação de exception

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -15,13 +15,23 @@ class NegociacaoController {
         'texto');
     }
     adiciona(event){
-        event.preventDefault();
+        try {
+            event.preventDefault();
 
-        this._negociacoes.adiciona(this._criaNegociacao());
+            this._negociacoes.adiciona(this._criaNegociacao());
 
-        this._mensagem.texto = "Negociação adicionada com sucesso!";
+            this._mensagem.texto = "Negociação adicionada com sucesso!";
 
-        this._limpaFormulario();
+            this._limpaFormulario();
+        }catch(err){
+            console.log(err);
+
+            if(err instanceof DataInvalidaException){
+                this._mensagem.texto = err.message;
+            }else {
+                this._mensagem.texto = 'Um erro inesperado aconteceu. Entre em contato com o suporte';
+            }
+        }
     }
 
     _criaNegociacao(){
